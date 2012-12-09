9556bcd63174ba98da8b3d6bfb7383ad2efe07d5
Varvara Stepanova <toivonen@toivonen.ru>
2012-12-09 19:25:13 +0400

    Start with md technology

--- .bem/make.js
+++ .bem/make.js
@@ -2,6 +2,9 @@
 
 "use strict";
 
+var BEM = require('bem'),
+    U = BEM.util;
+
 //process.env.YENV = 'production';
 
 MAKE.decl('Arch', {
@@ -28,6 +31,32 @@ MAKE.decl('BundleNode', {
             'md',
             'html'
         ];
+    }
+
+});
+
+MAKE.decl('BundleLevelNode', {
+
+    // см. https://github.com/bem/bem-tools/blob/master/lib/nodes/level.js
+    scanLevelItems: function() {
+
+        return _.uniq(
+            _.sortBy(
+                this.level.getItemsByIntrospection()
+                    .filter(function(item) {
+
+                        var type = U.bemType(item);  // U -> `BEM.util`
+
+                        if (type === 'block' && item.block === this.mergedBundleName()) return false;
+
+                        // NOTE: вместо bemjson.js и bemdecl.js указать свои технологии-источники !!!
+                        return ~['block', 'elem'].indexOf(type) && ~['md'].indexOf(item.tech);
+
+                    }, this),
+                U.bemKey),
+            true,
+            U.bemKey);
+
     }
 
 });
